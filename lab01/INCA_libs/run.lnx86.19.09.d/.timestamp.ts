dcb4b4cefa11eed3a10948c7a8f16a64ed079a6f
kacperoooni <kacpergaweda@gmail.com>
2020-11-18 21:12:07 +0100

    lab03 checkpoint

--- lab01/INCA_libs/run.lnx86.19.09.d/.timestamp.ts
+++
@@ -1,2 +0,0 @@
-1603911567 /home/student/kgaweda/VDIC/lab01/alu_testbench.sv
-1603040320 /home/student/kgaweda/VDIC/lab01/mtm_Alu.vp
--- lab01/INCA_libs/run.lnx86.19.09.d/files.ts
+++
@@ -1,2 +0,0 @@
-1603040320 /home/student/kgaweda/VDIC/lab01/mtm_Alu.vp
-1603911567 /home/student/kgaweda/VDIC/lab01/alu_testbench.sv
